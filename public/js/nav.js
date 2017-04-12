06dd8b17be9715450f2e66fc173cc4c31eb45e20
JonRoth <jonrothhh@gmail.com>
2017-04-11 23:14:24 -0700

    contact me page initial

--- public/js/nav.js
+++ public/js/nav.js
@@ -7,6 +7,8 @@ jQuery(document).ready(function($){
 		updateNavigation();
         changeNavColor();
         changeHeaderColor();
+        arrowChange();
+        revertHeader();
 	});
 
 	//smooth scroll to the section
@@ -31,6 +33,15 @@ jQuery(document).ready(function($){
         event.preventDefault();
         smoothScroll($(this.hash));
     });
+
+    //smooth scroll to contact me section
+      $('#contact_link').on('click', function(event){
+        event.preventDefault();
+        smoothScroll1($(this.hash));
+    });
+
+
+
 
     //open-close navigation on touch devices
     $('.touch .cd-nav-trigger').on('click', function(){
@@ -71,23 +82,87 @@ jQuery(document).ready(function($){
         });
     };
 
-function changeHeaderColor() {
-    contentSections.each(function(){
-            $this = $('#section1'); 
-            if ($(window).scrollTop() >= $('.cd-fixed-bg.cd-bg-1').height() - 70) {
-                $('.cd-header').addClass('menuColorChange');
-                $('.cd-main-nav a').addClass('hvr-bounce-to-bottom1 link_color_change');
-                $('#portfolio_link').focus();
-                $('#title1').css('color','#fff');
-            } else {
-                $('.cd-header').removeClass('menuColorChange');
-                $('.cd-main-nav a').removeClass('link_color_change');
-                $('.cd-main-nav a').removeClass('hvr-bounce-to-bottom1');
-                $('#title1').css('color','#546e7a');
-                $('#portfolio_link').blur();
-            }
+    function changeHeaderColor() {
+        contentSections.each(function(){
+                $this = $('#section1'); 
+                if ($(window).scrollTop() >= $('.cd-fixed-bg.cd-bg-1').height() - 70) {
+                    $('.cd-header').addClass('menuColorChange');
+                    $('.cd-main-nav a').addClass('hvr-bounce-to-bottom1 link_color_change');
+                    $('#portfolio_link').focus();
+                    $('#title1').css('color','#fff');
+                } else {
+                    $('.cd-header').removeClass('menuColorChange');
+                    $('.cd-main-nav a').removeClass('link_color_change');
+                    $('.cd-main-nav a').removeClass('hvr-bounce-to-bottom1');
+                    $('#title1').css('color','#546e7a');
+                    $('#portfolio_link').blur();
+                }
+            });
+        };
+
+    function arrowChange() {
+        contentSections.each(function(){
+                 if ($(window).scrollTop() >= $('.cd-main-content').height() + $('.cd-scrolling-bg.cd-color-2').height() + $('.cd-fixed-bg.cd-bg-2').height() + $('.cd-scrolling-bg.cd-color-3').height() 
+                        + $('.cd-fixed-bg.cd-bg-3').height() + $('.cd-scrolling-bg.cd-color-1').height() + ($('.cd-fixed-bg.cd-bg-4').height() * .50) - 70) {
+                    $('.arrow').addClass('out');
+                } else {
+                    $('.arrow').removeClass('out');
+
+                }
+            });
+        };
+
+    function revertHeader() {
+        contentSections.each(function(){
+                $this = $('#section1'); 
+                if ($(window).scrollTop() >= $('.cd-main-content').height() + $('.cd-scrolling-bg.cd-color-2').height() + $('.cd-fixed-bg.cd-bg-2').height() + $('.cd-scrolling-bg.cd-color-3').height() 
+                        + $('.cd-fixed-bg.cd-bg-3').height() + $('.cd-scrolling-bg.cd-color-1').height() + ($('.cd-fixed-bg.cd-bg-4').height()) - 70) {
+
+                    $('.cd-header').removeClass('menuColorChange');
+                    $('.cd-main-nav a').removeClass('link_color_change');
+                    $('.cd-main-nav a').removeClass('hvr-bounce-to-bottom1');
+                    $('#title1').css('color','#546e7a');
+                    $('#portfolio_link').blur();
+                    /*
+                    $('#github_icon').hover(
+                        function() {
+                           $('#github_icon').append('<h4>hello world</h4>');
+                        }); **/
+                };
+            });
+        };
+
+        //linkedin icon effect
+     $('#linkedin_icon').mouseenter(function() {
+            $('#linkedin_text').fadeTo(400,1);
+            $("#linkedin_text").addClass('hover');
         });
-    };
+     $('#linkedin_icon').mouseleave(function() {
+            $("#linkedin_text").removeClass('hover');
+            $('#linkedin_text').fadeTo(200,0);
+        });
+
+         //email icon effect
+     $('#mail_icon').mouseenter(function() {
+            $('#email_text').fadeTo(400,1);
+            $("#email_text").addClass('hover');
+        });
+     $('#mail_icon').mouseleave(function() {
+            $("#email_text").removeClass('hover');
+            $('#email_text').fadeTo(200,0);
+        });
+
+         //github icon effect
+     $('#github_icon').mouseenter(function() {
+            $('#github_text').fadeTo(400,1);
+            $("#github_text").addClass('hover');
+        });
+     $('#github_icon').mouseleave(function() {
+            $("#github_text").removeClass('hover');
+            $('#github_text').fadeTo(200,0);
+        });
+
+
 
 	function smoothScroll(target) {
         $('body,html').animate(
@@ -95,5 +170,13 @@ function changeHeaderColor() {
         	600
         );
 
-	}
+	};
+
+    function smoothScroll1(target) {
+        $('body,html').animate(
+            {'scrollTop':target.offset().top},
+            600
+        );
+
+    }
 });
