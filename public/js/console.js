da3aa61a53c894fcd6ffd12a630ad4583643ac09
JonRoth <jonrothhh@gmail.com>
2017-04-13 10:04:20 -0700

    more changes

--- public/js/console.js
+++ public/js/console.js
@@ -1,2 +1,2 @@
 console.log("%cWelcome to Designbyjon.com!",  "color: #5b7081; font-size: x-large");
-console.log("%c I built from the ground up as my first real exploration of front-end web design and development. Feel free to check out the source code over @ https://github.com/JonRothhh/portfolio",  "color: #5b7081;");
+console.log("%c I built this website from the ground up as my first real exploration of front-end web design and development. Feel free to check out the source code over @ https://github.com/JonRothhh/portfolio",  "color: #5b7081;");
