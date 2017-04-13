01fbc602c274dee76b96e95b1e0e19cbee90469f
JonRoth <jonrothhh@gmail.com>
2017-04-13 10:17:36 -0700

    console log greeting

--- public/js/console.js
+++ public/js/console.js
@@ -1,2 +1,3 @@
-console.log("%cWelcome to Designbyjon.com!",  "color: #5b7081; font-size: x-large");
-console.log("%c I built this website from the ground up as my first real exploration of front-end web design and development. Feel free to check out the source code over @ https://github.com/JonRothhh/portfolio",  "color: #5b7081;");
+console.log("%cWelcome to Designbyjon.com!",  "color: #546e7a; font-size: x-large; font-family: Sans-Serif");
+console.log("%cI built this website from the ground up as my first real exploration of front-end web design and development. Feel free to check out the source code over @ https://github.com/JonRothhh/portfolio",  "color: #5b7081;");
+
